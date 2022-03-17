70810dbbf174c46df504ca7e9b30b605d55a990e
Gijs Blanken <gijs@pop-os.localdomain>
2022-03-17 14:39:35 +0100

    [86] Initial work

--- src/App.vue
+++ src/App.vue
@@ -1,29 +1,18 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+  <package-table></package-table>
 </template>
 
 <script lang="ts">
 import { Options, Vue } from 'vue-class-component';
-import HelloWorld from './components/HelloWorld.vue';
+import PackageTable from './components/PackageTable.vue';
 
 @Options({
   components: {
-    HelloWorld,
+    PackageTable,
   },
 })
 export default class App extends Vue {}
 </script>
 
 <style>
-/*
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-*/
 </style>
