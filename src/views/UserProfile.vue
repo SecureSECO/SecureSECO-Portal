9a245727069df54f14258ecd00801c5835734fc1
Daan an der Spek <d.s.vanderspek@students.uu.nl>
2022-05-19 10:30:16 +0200

    Route done, start of profile view

--- src/views/UserProfile.vue
+++ src/views/UserProfile.vue
@@ -1,5 +1,9 @@
 <template>
   <h1 class="display-5">User profile</h1>
+  <table>
+    <tr><td>Name:</td><td><input type="text" value="Name" /></td></tr>
+    <tr><td>Setting:</td><td><input type="text" value="Name" /></td></tr>
+  </table>
 </template>
 
 <script>
