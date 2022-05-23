7f0cd83124359c50f3bb2ec3fa97d954ee1ed08c
Daan an der Spek <d.s.vanderspek@students.uu.nl>
2022-05-23 19:35:28 +0200

    Added right settings for user profile, made component structure

--- src/views/UserProfile.vue
+++ src/views/UserProfile.vue
@@ -1,16 +1,18 @@
 <template>
   <h1 class="display-5">User profile</h1>
-  <table>
-    <tr><td>Name:</td><td><input type="text" value="Name" /></td></tr>
-    <tr><td>Setting:</td><td><input type="text" value="Name" /></td></tr>
-  </table>
+  <user-profile-component/>
 </template>
 
 <script>
+import UserProfileComponent from '../components/UserProfileComponent.vue';
+
 export default {
   name: 'user-profile-view',
+  components: { UserProfileComponent },
 };
 </script>
 
 <style scoped>
 </style>
+
+
