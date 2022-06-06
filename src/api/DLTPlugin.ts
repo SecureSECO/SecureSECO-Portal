f2bc36ec8565a84bf9e6cfdca27d0fbbeda98fb6
Youri <y.bok@students.uu.nl>
2022-06-07 01:51:50 +0200

    Update API types and add `getPackageNames`

--- src/api/DLTPlugin.ts
+++ src/api/DLTPlugin.ts
@@ -1,16 +1,39 @@
 import { App } from 'vue';
 import { GlobalConfig } from 'vuestic-ui';
 
+export const defaultPackage = {
+  id: 0,
+  platform: 'Github',
+  owner: 'Fides',
+  name: 'Portal',
+  release: 'v1.2.3',
+  score: 0,
+  updatedAt: new Date(Date.now() - 24 * 60 * 60 * 1000),
+};
+
 export interface Package {
   id: number,
+  platform: string,
+  owner: string,
   name: string,
+  release: string,
   score: number,
+  updatedAt: Date,
+}
+
+export interface TrustFact {
+  type: string,
+  value: number,
 }
 
 export abstract class DLTPlugin {
-  abstract retrievePackages(): Package[];
+  abstract getPackageNames(): string[];
 
-  abstract retrievePackage(id: number): Package;
+  abstract getPackages(): Package[];
+
+  abstract getPackage(id: number): Package;
+
+  abstract getTrustFacts(id: number): TrustFact[];
 
   install(app: App, config: GlobalConfig) {
     // eslint-disable-next-line no-param-reassign
--- src/api/Mocking.ts
+++ src/api/Mocking.ts
@@ -1,27 +1,46 @@
 /* eslint-disable class-methods-use-this */
-import { DLTPlugin, Package } from '@/api/DLTPlugin';
+import { defaultPackage, DLTPlugin, Package, TrustFact } from '@/api/DLTPlugin';
 
 export default class Mocking extends DLTPlugin {
-  retrievePackage(id: number): Package {
-    const score = id;
-    const name = `Package ${score}`;
-
-    return {
-      id,
-      name,
-      score,
-    };
+  getPackageNames(): string[] {
+    const packageNames = [];
+    for (let id = 0; id < 100; id += 1) {
+      packageNames[id] = `Package ${id}`;
+    }
+    return packageNames;
   }
 
-  retrievePackages(): Package[] {
+  getPackages(): Package[] {
     const packages: Package[] = [];
     for (let id = 0; id < 100; id += 1) {
       packages[id] = {
+        ...defaultPackage,
         id,
         name: `Package ${id}`,
         score: id,
+        updatedAt: new Date(Date.now() - id * 24 * 60 * 60 * 1000),
       };
     }
     return packages;
+  }
+
+  getPackage(id: number): Package {
+    return {
+      ...defaultPackage,
+      id,
+      name: `Package ${id}`,
+      score: id,
+    };
+  }
+
+  getTrustFacts(id: number): TrustFact[] {
+    const trustFacts = [];
+    for (let i = 0; i < id; i += 1) {
+      trustFacts[i] = {
+        type: 'github stars',
+        value: i,
+      };
+    }
+    return trustFacts;
   }
 }
