3d0f6687aa3abd18355f8ba2d4009ddd5f6c5dbc
Daan an der Spek <d.s.vanderspek@students.uu.nl>
2022-06-14 14:11:43 +0200

    Storybook init

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,36 +1,26 @@
 module.exports = {
   root: true,
   env: {
-    es2021: true,
+    es2021: true
   },
-  extends: [
-    'plugin:vue/vue3-essential',
-    '@vue/airbnb',
-    '@vue/typescript/recommended',
-  ],
+  extends: ['plugin:vue/vue3-essential', '@vue/airbnb', '@vue/typescript/recommended', 'plugin:storybook/recommended'],
   settings: {
     'import/resolver': {
       "typescript": {}
     }
   },
   parserOptions: {
-    ecmaVersion: 2020,
+    ecmaVersion: 2020
   },
   rules: {
     'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
     'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
-
-    'max-len': 'off',
+    'max-len': 'off'
   },
-  overrides: [
-    {
-      files: [
-        '**/__tests__/*.{j,t}s?(x)',
-        '**/tests/unit/**/*.spec.{j,t}s?(x)',
-      ],
-      env: {
-        jest: true,
-      },
-    },
-  ],
+  overrides: [{
+    files: ['**/__tests__/*.{j,t}s?(x)', '**/tests/unit/**/*.spec.{j,t}s?(x)'],
+    env: {
+      jest: true
+    }
+  }]
 };
