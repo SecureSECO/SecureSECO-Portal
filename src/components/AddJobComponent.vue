61935aab78576d9f9747983d75240acd122040f0
Youri <y.bok@students.uu.nl>
2022-06-14 23:41:02 +0200

    Run `npm run lint` and fix linting errors

--- src/components/AddJobComponent.vue
+++ src/components/AddJobComponent.vue
@@ -26,7 +26,6 @@
 
 <script lang="ts">
 import { defineComponent } from 'vue';
-import axios from 'axios';
 
 export default defineComponent({
   name: 'add-package-component',
--- src/components/button/DownloadCoSy.vue
+++ src/components/button/DownloadCoSy.vue
@@ -4,7 +4,6 @@
 
 <script lang="ts">
 import { defineComponent } from 'vue';
-import axios from 'axios';
 
 export default defineComponent({
   name: 'download-cosy-button',
